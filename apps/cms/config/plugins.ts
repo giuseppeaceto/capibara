494bfce29d1448c0e905d5640c54461257037047
Giuseppe Aceto <giuseppe-aceto@hotmail.com>
2025-12-02 15:57:06 +0100

    Remove wysiwyg-react-md-editor plugin from CMS
    The wysiwyg-react-md-editor plugin configuration and dependency have been removed from the CMS app. This cleans up unused code and dependencies, likely in preparation for a different rich text editor or to simplify the plugin setup.

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -1,14 +1,2 @@
-export default () => ({
-  // Editor WYSIWYG basato su React MD Editor per i campi rich text
-  "wysiwyg-react-md-editor": {
-    enabled: true,
-    config: {
-      // Toolbar di base, puoi personalizzarla da qui secondo le tue esigenze:
-      // ["title2", "title3", "bold", "italic", "quote", "unorderedListCommand", "orderedListCommand", "strapiMediaLibrary"]
-      // Se la lasci vuota, il plugin userà la toolbar di default.
-      // vedi: https://market.strapi.io/plugins/strapi-plugin-wysiwyg-react-md-editor
-      // toolbarCommands: ["title2", "title3", "bold", "italic", "quote", "unorderedListCommand", "orderedListCommand", "strapiMediaLibrary"],
-    },
-  },
-});
+export default () => ({});
 
